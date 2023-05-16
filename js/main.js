f36b7039beaf0ba6a66d25e190bf43a9550ec88f
mamadou4002 <112548633+mamadou4002@users.noreply.github.com>
2023-05-16 12:35:51 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,84 +1,122 @@
-const images = document.querySelectorAll('.img-box');
+let wins=0
+let computerwins=0
+
 
 //default settings
 let turn = false;
+//turn=false betekent speler
+//turn=true betekent computer
 
 function placeXO(data) {
-   // console.log(data);
-    if(data.target.attributes[0].nodeValue == "img/blank.png") {
-        if(turn == false) {
+    // console.log(data);
+    if (data.target.attributes[0].nodeValue == "img/blank.png") {
+        if (turn == false) {
             data.target.attributes[0].nodeValue = "img/x.png"
             turn = true;
-            document.getElementById("turn").innerText= "o turn"
+            document.getElementById("turn").innerText = "o turn"
+            ComputerMove()
+
         }
-        else{
-            data.target.attributes[0].nodeValue = "img/o.png"
-            turn = false;
-            document.getElementById("turn").innerText="x turn"
-        }
+        
     }
-   // console.log(images[0].src);
+    // console.log(images[0].src);
     checkWin();
 }
+function ComputerMove(){
+    let allboxes=document.querySelectorAll(".img-box")
+    let emptyboxes=[]
 
-function checkWin(){
+    for (let i=0;i< allboxes.length;i++){
+        if(allboxes[i].src.includes("blank")){
+            emptyboxes.push(allboxes[i]);
+        }
+    }
 
-     const boxes = document.querySelectorAll('.img-box');
-     console.log(boxes)
-     if (boxes[0].src== '/css tic tak to/img/x.png' && 
-            boxes[1].src== '/css tic tak to/img/x.png' &&
-            boxes[2].src== '/css tic tak to/img/x.png' ) {
-            console.log("x wins")
+    console.log(emptyboxes)
+    emptyboxes[Math.floor(Math.random() * emptyboxes.length)].src="img/o.png"
+    turn=false
+}
+
+function checkWin() {
+
+    const boxes = document.querySelectorAll('.img-box');
+    if (boxes[0].src == 'http://127.0.0.1:5500/img/x.png' &&
+        boxes[1].src == 'http://127.0.0.1:5500/img/x.png' &&
+        boxes[2].src == 'http://127.0.0.1:5500/img/x.png') {
+        console.log("x wins")
+        resetGame(true)
+    }
+
+    if (boxes[3].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[4].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[5].src == "http://127.0.0.1:5500/img/x.png") {
+        console.log("x wins")
+        resetGame(true)
+    }
+    if (boxes[6].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[7].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[8].src == "http://127.0.0.1:5500/img/x.png") {
+        console.log("x wins")
+        resetGame(true)
+    }
+
+   
+    if (boxes[0].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[4].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[8].src == "http://127.0.0.1:5500/img/x.png") {
+        console.log("x wins")
+        resetGame(true)
+    }
+    if (boxes[2].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[4].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[6].src == "http://127.0.0.1:5500/img/x.png") {
+        console.log("x wins")
+        resetGame(true)
+    }
+
+    if (boxes[0].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[3].src == "http://127.0.0.1:5500/img/x.png" &&
+        boxes[6].src == "http://127.0.0.1:5500/img/x.png") {
+        console.log("x wins")
+        resetGame(true)
+    }
+
+    let isPossible=false
+    for (let i = 0; i < boxes.length; i++) {
+        if(boxes[i].src.toString().includes("blank")){
+            isPossible=true
         }
 
-    if(boxes[3].src=="/css tic tak to/img/x.png" &&
-            boxes[4].src=="/css tic tak to/img/x.png" &&
-            boxes[5].src=="/css tic tak to/img/x.png" ){ 
-            console.log("x wins")
-            }
-    if(boxes[6].src=="/css tic tak to/img/x.png" &&
-             boxes[7].src=="/css tic tak to/img/x.png" &&
-             boxes[8].src=="/css tic tak to/img/x.png"){
-             console.log("x wins")
-        }
+    }
+    if(isPossible==false){
+        resetGame()
+    }
+
+    document.getElementById("wins").innerText=wins
+    document.getElementById("computerwins").innerText=computerwins
+
     
-     for (let i = 0; i < boxes.length; i++) {
-        console.log( i + ' ' + boxes[i].src);
-
-     }
-     if(boxes[0].src=="/css tic tak to/img/x.png" &&
-     boxes[4].src=="/css tic tak to/img/x.png" &&
-     boxes[8].src=="/css tic tak to/img/x.png"){
-     console.log("x wins")
 }
-if(boxes[2].src=="/css tic tak to/img/x.png" &&
-             boxes[4].src=="/css tic tak to/img/x.png" &&
-             boxes[6].src=="/css tic tak to/img/x.png"){
-             console.log("x wins")
-        }
 
-        if(boxes[0].src=="/css tic tak to/img/x.png" &&
-             boxes[3].src=="/css tic tak to/img/x.png" &&
-             boxes[6].src=="/css tic tak toe/img/x.png"){
-             console.log("x wins")
+function resetGame(gewonnen){
+    const boxes=document.querySelectorAll(".img-box")
+
+    for(let i =0;i<boxes.length;i++){
+        boxes[i].src="img/blank.png"
+       boxes[i].addEventListener("click",placeXO)
+    }
+    turn=false
+
+    if (gewonnen != null) {
+        if (gewonnen==true) {
+            wins+=1
         }
+        
+        if(gewonnen==false){
+            computerwins+=1
+        }
+    }
     
-     for (let i = 0; i < boxes.length; i++) {
-        console.log( i + ' ' + boxes[i].src);
-
-     }
-
-for (let i = 0; i < boxes.length; i++) {
-console.log( i + ' ' + boxes[i].src);
-
-}
-}
-for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener('click', placeXO)
 }
 
-while(images[0].src == "/css tic tak to/img/x.png"){
-    console.log("jah");
-}
-
-console.log('loaded');
+resetGame()
